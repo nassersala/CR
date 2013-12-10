9116b34f0674a0e74ab0ba517470f09bc634b255
Nasser Al Zahrani <nassersala@gmail.com>
2013-12-10 20:35:22 +1100

    the first step in world domination is making a commitment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoderetreatLive::Application.config.secret_key_base = 'dfa5f48e53feb57b4ea4fc16ebb35c52ae5077c138e7a781bac844ba882bf358a03a47a2f64af10aff3704cee318452cd74ed0339198375336f71b8d73e51412'
