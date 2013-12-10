be219ae05958a702306f380f5b8f7854b6f8eff2
Nasser Al Zahrani <nassersala@gmail.com>
2013-12-11 10:43:46 +1100

    Coderetreats collection presenter cleanup: generate status methods

--- spec/coderetreat_live/coderetreats/presenters/collection_spec.rb
+++ spec/coderetreat_live/coderetreats/presenters/collection_spec.rb
@@ -2,19 +2,16 @@ require 'coderetreats/presenters/collection'
 
 describe CoderetreatLive::Coderetreats::Presenters::Collection do
   context "getting the coderetreats goruped by status" do
+    let(:not_started) { double("not_started", status: "not_started") }
+    let(:in_session) { double("in_session", status: "in_session") }
+    let(:coderetreats) { [not_started, in_session] }
+    let(:presenter) { CoderetreatLive::Coderetreats::Presenters::Collection.for(coderetreats) }
+
     it "exposes those in status not_started" do
-      not_started = double("not_started", status: "not_started")
-      in_session = double("in_session", status: "in_session")
-      coderetreats = [not_started, in_session]
-      presenter = CoderetreatLive::Coderetreats::Presenters::Collection.for(coderetreats)
       expect{|b| presenter.not_started(&b) }.to yield_successive_args(not_started)
     end
 
     it "exposes those in status in_session" do
-      not_started = double("not_started", status: "not_started")
-      in_session = double("in_session", status: "in_session")
-      coderetreats = [not_started, in_session]
-      presenter = CoderetreatLive::Coderetreats::Presenters::Collection.for(coderetreats)
       expect{|b| presenter.in_session(&b) }.to yield_successive_args(in_session)
     end
   end
