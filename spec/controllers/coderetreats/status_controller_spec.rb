df7d21f57687298c26cecbc8cb47937d1b758c04
Nasser Ali AlZahrani <nassersala@gmail.com>
2013-12-21 20:39:44 +1100

    Update a CR Status is now done through the status resource

--- spec/controllers/coderetreats/status_controller_spec.rb
+++ spec/controllers/coderetreats/status_controller_spec.rb
@@ -11,4 +11,19 @@ describe Coderetreats::StatusController do
     end
   end
 
+  describe "PUT /update" do
+    before do
+      CoderetreatLive::Coderetreats::Status.stub(:update_to)
+    end
+
+    it "redirects back to  the edit status page" do
+      put :update, coderetreat_id: "5", new_status: "in_session"
+      expect(response).to redirect_to(edit_coderetreat_status_url("5"))
+    end
+
+    it "updates the status of a coderetreat" do
+      CoderetreatLive::Coderetreats::Status.should_receive(:update_to).with("5", "in_session")
+      put :update, coderetreat_id: "5", new_status: "in_session"
+    end
+  end
 end
