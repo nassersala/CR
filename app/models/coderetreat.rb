60698334ded722b0b4b0bdcaf3eb68324670261f
Nasser Ali AlZahrani <nassersala@gmail.com>
2013-12-22 20:17:21 +1100

    push knowledge of statuses from edit view into model

--- app/models/coderetreat.rb
+++ app/models/coderetreat.rb
@@ -1,9 +1,14 @@
 class Coderetreat < ActiveRecord::Base
+
   def self.running_today
     all
   end
 
   def update_status(new_status)
     self.update_attributes status: new_status
+  end
+
+  def available_next_statuses
+   ["not_started", "in_session", "on_break"] - [self.status]
   end
 end
