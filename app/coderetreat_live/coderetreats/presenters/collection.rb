fc230a7354b781c19be61ea80cb84729153ca341
Nasser Ali AlZahrani <nassersala@gmail.com>
2013-12-22 14:54:35 +1100

    Coderetrats collection presenter yielding the coderetreats for a status

--- app/coderetreat_live/coderetreats/presenters/collection.rb
+++ app/coderetreat_live/coderetreats/presenters/collection.rb
@@ -15,8 +15,12 @@ module CoderetreatLive
           ["not_started", "in_session", "on_break"]
         end
 
+        def filtered_by_status(status)
+          self.coderetreats.select { |cr| cr.status == status }
+        end
+
         def grouped_by_status(&block)
-         self.class.all_statuses.each {|status| yield status, []}
+         self.class.all_statuses.each {|status| yield status, filtered_by_status(status).each(&block) }
         end
 
         self.all_statuses.each do |status|
